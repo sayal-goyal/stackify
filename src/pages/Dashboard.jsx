0b61a622d8fb96a19870df2dc21368bed9bcc3b4
sayal-goyal <sayalagoyal2003@gmail.com>
2025-01-13 20:05:45 +0530

    fix

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -6,7 +6,7 @@ const Dashboard = () => {
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-100">
       <Link to={`https://slack.com/oauth/v2/authorize?client_id=${clientId}&scope=chat:write,channels:read&redirect_uri=https://stackify.vercel.app/user/auth/slack`}
-        className="px-8 py-3 bg-gradient-to-r from-blue-600 to-blue-400 text-white rounded-lg shadow-lg">Integrate to Slack</Link>
+        className="px-8 py-3 bg-gradient-to-r from-blue-600 to-blue-400 text-white rounded-lg shadow-lg hover:brightness-105">Integrate to Slack</Link>
     </div >
   );
 };
