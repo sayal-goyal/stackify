f8d0bc947be2c3bba7420609282b1a6bbbd2b9be
sayal-goyal <sayalagoyal2003@gmail.com>
2025-01-15 18:18:50 +0530

    fix

--- src/pages/user/auth/SlackAuth.jsx
+++ src/pages/user/auth/SlackAuth.jsx
@@ -3,12 +3,39 @@ import { useLocation } from 'react-router-dom';
 
 const SlackAuth = () => {
   const location = useLocation();
+  const queryParams = new URLSearchParams(location.search);
 
-  const params = new URLSearchParams(location.search);
-  const codeName = params.get('code');
+  const fetchToken = async () => {
+    try {
+      const response = await fetch('https://login.microsoftonline.com/common/oauth2/v2.0/token', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${process.env.REACT_APP_SLACK_BOT_TOKEN}`
+        },
+        body: {
+          'client_id': clientId,
+          'client_secret': clientSecret,
+          'code': queryParams.get('code'),
+          'redirect_uri': 'https://stackify.vercel.app/user/slack',
+          // 'grant_type'=authorization_code,
+        }
+      });
+
+      const data = await response.json();
+      if (response.ok) console.log('Token response:', data);
+      else console.error('Error response:', data);
+    } catch (error) {
+      console.error('Fetch error:', error);
+    }
+  };
+
+  useEffect(() => {
+    fetchToken();
+  }, [])
 
   return (
-    <div>Slack</div>
+    <div>Slack Loading</div>
   )
 }
 
