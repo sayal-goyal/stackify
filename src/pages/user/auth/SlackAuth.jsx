99ef86028d951e2c073cd5f5b07b4f8ff6f772ae
sayal-goyal <sayalagoyal2003@gmail.com>
2025-01-15 18:37:51 +0530

    fix

--- src/pages/user/auth/SlackAuth.jsx
+++ src/pages/user/auth/SlackAuth.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useLocation } from 'react-router-dom';
 const clientId = import.meta.env.VITE_CLIENT_ID;
 const clientSecret = import.meta.env.VITE_CLIENT_SECRET;
