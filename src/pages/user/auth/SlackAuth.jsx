dcf615cd1a90e100e39362f45d00fb597a28a583
sayal-goyal <sayalagoyal2003@gmail.com>
2025-01-15 18:56:41 +0530

    fix

--- src/pages/user/auth/SlackAuth.jsx
+++ src/pages/user/auth/SlackAuth.jsx
@@ -11,19 +11,21 @@ const SlackAuth = () => {
   useEffect(() => {
     const fetchToken = async () => {
       try {
+        const body = {
+          'client_id': clientId,
+          'client_secret': clientSecret,
+          'code': queryParams.get('code'),
+          'redirect_uri': 'https://stackify.vercel.app/user/slack',
+          // 'grant_type'=authorization_code,
+        }
+
         const response = await fetch('https://login.microsoftonline.com/common/oauth2/v2.0/token', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
             'Authorization': `Bearer ${process.env.REACT_APP_SLACK_BOT_TOKEN}`
           },
-          body: {
-            'client_id': clientId,
-            'client_secret': clientSecret,
-            'code': queryParams.get('code'),
-            'redirect_uri': 'https://stackify.vercel.app/user/slack',
-            // 'grant_type'=authorization_code,
-          }
+          body: JSON.stringify(body)
         });
 
         const data = await response.json();
