c32e630ae16fca08284192225b85fc2aa45b08fc
sayal-goyal <sayalagoyal2003@gmail.com>
2025-01-15 18:21:36 +0530

    fix

--- src/pages/user/Slack.jsx
+++ src/pages/user/Slack.jsx
@@ -1,12 +1,9 @@
 import React from 'react'
-import { useLocation } from 'react-router-dom';
-const clientId = process.env.REACT_APP_CLIENT_ID;
-const clientSecret = process.env.REACT_APP_CLIENT_SECRET;
 
 const Slack = () => {
     return (
         <div>
-slack
+            slack
         </div>
     )
 }
--- src/pages/user/auth/SlackAuth.jsx
+++ src/pages/user/auth/SlackAuth.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import { useLocation } from 'react-router-dom';
+const clientId = process.env.REACT_APP_CLIENT_ID;
+const clientSecret = process.env.REACT_APP_CLIENT_SECRET;
 
 const SlackAuth = () => {
   const location = useLocation();
