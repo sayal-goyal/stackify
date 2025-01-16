6c60f914acce5a045e9117946c02ef88ef19fd71
sayal-goyal <sayalagoyal2003@gmail.com>
2025-01-16 18:21:22 +0530

    add: chanels

--- src/pages/user/Slack.jsx
+++ src/pages/user/Slack.jsx
@@ -1,10 +1,12 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
+import { useNavigate } from 'react-router-dom';
 import Select from 'react-select'
 const baseURL = import.meta.env.VITE_BACKEND_URL;
 
 const Slack = () => {
+    const channelsRef = useRef();
+    const navigate = useNavigate();
     const [channels, setChannels] = useState([]);
-    const [selectedChannels, setSelectedChannels] = useState();
 
 
     useEffect(() => {
@@ -15,7 +17,7 @@ const Slack = () => {
                 });
 
                 const data = await response.json();
-                if (response.ok) setChannels(data);
+                if (response.ok) setChannels(data.data);
                 else console.error('Error response:', data);
             } catch (error) {
                 console.error('Fetch error:', error);
@@ -24,33 +26,32 @@ const Slack = () => {
         fetchList();
     }, []);
 
-    const handleSubmit = async () => {
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const selectedOptions = channelsRef.current?.getValue() || [];
+        const selectedChannelIds = selectedOptions.map(option => option.value);
+
         try {
-            console.log(selectedChannels)
-            const response = await fetch(baseURL + '/user/slack/sendChannels', {
+            const response = await fetch(baseURL + '/user/slack/updateChannels?username=username', {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
-                body: JSON.stringify({ channelIds: selectedChannels }),
+                body: JSON.stringify({ channelIds: selectedChannelIds }),
             });
 
             const data = await response.json();
-            if (response.ok) {
-                console.log('Successfully sent channel IDs:', data);
-            } else {
-                console.error('Error response:', data);
-            }
+            if (response.ok) navigate('/dashboard')
+            else console.error('Error response:', data);
         } catch (error) {
             console.error('Submit error:', error);
         }
     };
 
-    const handleChange = (selectedList) => setSelectedChannels(selectedList)
 
     return (
         <form onSubmit={handleSubmit} className="flex flex-col items-center justify-center min-h-screen bg-gray-100 gap-5">
             <div className='flex flex-col gap-3'>
                 <h2 className='text-slate-700'>Select Channels</h2>
-                <Select options={channels?.map(({ id, name }) => ({ label: name, value: id }))} value={selectedChannels} handleChange={handleChange} />
+                <Select ref={channelsRef} options={channels?.map(({ id, name }) => ({ label: name, value: id }))} isMulti />
 
                 <button type="submit" className="px-8 py-2 bg-gradient-to-r from-blue-600 to-blue-400 text-white rounded-lg shadow-lg hover:brightness-105 w-96 mt-3">
                     Submit Channels
