c47aedd30674005914b7a7da673ce916c2e458f1
sayal-goyal <sayalagoyal2003@gmail.com>
2025-01-20 19:11:43 +0530

    fix

--- src/pages/user/auth/TeamsAuth.jsx
+++ src/pages/user/auth/TeamsAuth.jsx
@@ -4,37 +4,37 @@ const baseURL = import.meta.env.VITE_BACKEND_URL;
 
 
 const TeamsAuth = () => {
-  const location = useLocation();
-  const navigate = useNavigate();
-  const queryParams = new URLSearchParams(location.search);
+    const location = useLocation();
+    const navigate = useNavigate();
+    const queryParams = new URLSearchParams(location.search);
 
-  useEffect(() => {
-    const fetchToken = async () => {
-      try {
-        const response = await fetch(baseURL + '/user/auth/teams', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({ username: 'username', code: queryParams.get('code') })
-        });
+    useEffect(() => {
+        const fetchToken = async () => {
+            try {
+                const response = await fetch(baseURL + '/user/auth/teams', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify({ username: 'username', code: queryParams.get('code') })
+                });
 
-        const data = await response.json();
-        if (response.ok) navigate('/user/teams');
-        else console.error('Error response:', data);
-      } catch (error) {
-        console.error('Fetch error:', error);
-      }
-    };
+                const data = await response.json();
+                if (response.ok) navigate('/user/teams');
+                else console.error('Error response:', data);
+            } catch (error) {
+                console.error('Fetch error:', error);
+            }
+        };
 
-    fetchToken();
-  }, [])
+        // fetchToken();
+    }, [])
 
-  return (
-    <div className='flex min-h-screen justify-center items-center text-center text-2xl text-slate-500'>
-      Processing Request......
-    </div>
-  )
+    return (
+        <div className='flex min-h-screen justify-center items-center text-center text-2xl text-slate-500'>
+            Processing Request......
+        </div>
+    )
 }
 
 export default TeamsAuth;
