8416c518f6bde4aaab86ef2d1a70264b179f0e11
sayal-goyal <sayalagoyal2003@gmail.com>
2025-01-22 16:27:48 +0530

    fix

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -9,7 +9,7 @@ const Dashboard = () => {
 
   const handleSlackSubmit = (e) => {
 
-    const redirectUri = `https://slack.com/oauth/v2/authorize?client_id=${slackClientId}&scope=channels:manage,channels:read,channels:write.invites,chat:write,groups:read,groups:write,groups:write.invites,im:read,incoming-webhook,users:read,channels:join&redirect_uri=${baseURL}/user/auth/slack`;
+    const redirectUri = `https://slack.com/oauth/v2/authorize?client_id=${slackClientId}&scope=channels:manage,channels:read,channels:write.invites,chat:write,groups:read,groups:write,groups:write.invites,im:read,incoming-webhook,users:read,channels:join&user_scope=groups:read,groups:write,groups:write.invites&redirect_uri=${baseURL}/user/auth/slack`;
     window.location.href = redirectUri;
   };
 
