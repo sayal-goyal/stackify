0d3b94ebc7ebe8330efd877307f9f6d10329acfd
sayal-goyal <sayalagoyal2003@gmail.com>
2025-01-22 16:39:19 +0530

    fix

--- src/pages/user/Slack.jsx
+++ src/pages/user/Slack.jsx
@@ -1,13 +1,11 @@
 import React, { useState, useEffect, useRef } from 'react';
 import { useNavigate } from 'react-router-dom';
-import Select from 'react-select'
 const baseURL = import.meta.env.VITE_BACKEND_URL;
 
 const Slack = () => {
-    const channelsRef = useRef();
     const navigate = useNavigate();
     const [channels, setChannels] = useState([]);
-
+    const [selectedChannel, setSelectedChannel] = useState('');
 
     useEffect(() => {
         const fetchList = async () => {
@@ -28,14 +26,12 @@ const Slack = () => {
 
     const handleSubmit = async (e) => {
         e.preventDefault();
-        const selectedOptions = channelsRef.current?.getValue() || [];
-        const selectedChannelIds = selectedOptions.map(option => option.value);
 
         try {
             const response = await fetch(baseURL + '/user/slack/updateChannels?username=username', {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
-                body: JSON.stringify({ channelIds: selectedChannelIds }),
+                body: JSON.stringify({ channel: selectedChannel }),
             });
 
             const data = await response.json();
@@ -51,7 +47,16 @@ const Slack = () => {
         <form onSubmit={handleSubmit} className="flex flex-col items-center justify-center min-h-screen bg-gray-100 gap-5">
             <div className='flex flex-col gap-3'>
                 <h2 className='text-slate-700'>Select Channels</h2>
-                <Select ref={channelsRef} options={channels?.map(({ id, name }) => ({ label: name, value: id }))} isMulti />
+                <select id="channel" name='channel' value={selectedChannel} onChange={(e) => setSelectedChannel(e.target.value)} aria-placeholder='Select Channel' className='h-11 bg-white rounded-md p-1 border-blue-100'>
+                    <option value="" disabled>
+                        Select Channel
+                    </option>
+                    {channels.map((option, index) => (
+                        <option key={index} value={option.id}>
+                            {option.name}
+                        </option>
+                    ))}
+                </select>
 
                 <button type="submit" className="px-8 py-2 bg-gradient-to-r from-blue-600 to-blue-400 text-white rounded-lg shadow-lg hover:brightness-105 w-96 mt-3">
                     Submit Channels
