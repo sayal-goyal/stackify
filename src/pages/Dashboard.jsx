94b35d75b81067488a6c1bec800372bb6ccc1fd3
sayal-goyal <sayalagoyal2003@gmail.com>
2025-01-23 14:54:22 +0530

    fix

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -8,13 +8,11 @@ const Dashboard = () => {
   const [username, setUsername] = useState('');
 
   const handleSlackSubmit = (e) => {
-
     const redirectUri = `https://slack.com/oauth/v2/authorize?client_id=${slackClientId}&scope=channels:manage,channels:read,channels:write.invites,chat:write.public,chat:write,groups:read,groups:write,groups:write.invites,im:read,incoming-webhook,users:read,channels:join&redirect_uri=${baseURL}/user/auth/slack&state=${username}`;
     window.location.href = redirectUri;
   };
 
   const handleTeamsSubmit = (e) => {
-
     const redirectUri = `https://login.microsoftonline.com/common/oauth2/v2.0/authorize
       ?client_id=${teamsClientId}&response_type=code&response_mode=query&redirect_uri=${baseURL}/user/auth/teams&state=${username}
       &scope=openid offline_access https://graph.microsoft.com/ChannelMessage.Read.All https://graph.microsoft.com/Chat.ReadWrite.All https://graph.microsoft.com/Group.Read.All https://graph.microsoft.com/Group.ReadWrite.All https://graph.microsoft.com/Team.ReadBasic.All https://graph.microsoft.com/User.Read.All https://graph.microsoft.com/User.ReadBasic.All https://graph.microsoft.com/User.ReadWrite.All
@@ -24,7 +22,7 @@ const Dashboard = () => {
 
   return (
     <form className="flex flex-col items-center justify-center min-h-screen bg-gray-100 gap-5">
-      <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} placeholder="Enter your name" className="px-4 py-2 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-400 w-96" />
+      <input required type="text" value={username} onChange={(e) => setUsername(e.target.value)} placeholder="Enter your name" className="px-4 py-2 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-400 w-96" />
       <button type='submit' onClick={handleSlackSubmit} className="px-8 py-2 bg-gradient-to-r from-blue-600 to-blue-400 text-white rounded-lg shadow-lg hover:brightness-105 w-96">
         Integrate with Slack
       </button>
