57546f66f6ade993df6efc8681a564a10ff179b6
sayal-goyal <sayalagoyal2003@gmail.com>
2025-01-23 15:33:34 +0530

    fix

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -21,15 +21,15 @@ const Dashboard = () => {
   };
 
   return (
-    <form className="flex flex-col items-center justify-center min-h-screen bg-gray-100 gap-5">
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 gap-5">
       <input required type="text" value={username} onChange={(e) => setUsername(e.target.value)} placeholder="Enter your name" className="px-4 py-2 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-400 w-96" />
-      <button type='submit' onClick={handleSlackSubmit} className="px-8 py-2 bg-gradient-to-r from-blue-600 to-blue-400 text-white rounded-lg shadow-lg hover:brightness-105 w-96">
+      <button disabled={username === ''} onClick={handleSlackSubmit} className="px-8 py-2 bg-gradient-to-r from-blue-600 to-blue-400 text-white rounded-lg shadow-lg hover:brightness-105 w-96">
         Integrate with Slack
       </button>
-      <button type='submit' onClick={handleTeamsSubmit} className="px-8 py-2 bg-gradient-to-r from-blue-600 to-blue-400 text-white rounded-lg shadow-lg hover:brightness-105 w-96">
+      <button disabled={username === ''} onClick={handleTeamsSubmit} className="px-8 py-2 bg-gradient-to-r from-blue-600 to-blue-400 text-white rounded-lg shadow-lg hover:brightness-105 w-96">
         Integrate with Teams
       </button>
-    </form>
+    </div>
   );
 };
 
